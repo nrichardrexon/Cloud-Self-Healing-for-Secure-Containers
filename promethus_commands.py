ab26ebf865996b519b07c83abbd1d43271c11200
Ragul2526 <88706335+Ragul2526@users.noreply.github.com>
2025-10-26 10:02:57 +0000

    added remediator.py but its not working, main changes are the self_healing_rules(prometheus)  and alert_manager ,run promethus_commands.py and check the commands.txt in monitoring1

--- promethus_commands.py
+++ promethus_commands.py
@@ -1,13 +1,13 @@
 import subprocess
-
+#comment out the first 3 if helm is not installed
 commands = [
-    "helm repo add prometheus-community https://prometheus-community.github.io/helm-charts",
-    "helm repo update",
-    "helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring --create-namespace",
-    "kubectl apply -f monitoring/prometheus.yaml -n monitoring",
-    "kubectl apply -f monitoring/alertmanager.yaml -n monitoring",
-    "kubectl apply -f monitoring/alert_rules.yaml -n monitoring",
-    "docker run -d   -p 9090:9090   -v /workspaces/Cloud-Self-Healing-for-Secure-Containers/monitoring/prometheus.yaml:/etc/prometheus/prometheus.yml   -v /workspaces/Cloud-Self-Healing-for-Secure-Containers/monitoring/alert_rules.yml:/etc/prometheus/alert_rules.yml   prom/prometheus:v2.52.0",
+    #"helm repo add prometheus-community https://prometheus-community.github.io/helm-charts",
+    #"helm repo update",
+    #"helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring --create-namespace",
+    "kubectl apply -f monitoring1/self-healing-rules.yaml",
+    "kubectl apply -f monitoring1/alertmanager.yaml -n monitoring",
+    
+    #"docker run -d   -p 9090:9090   -v /workspaces/Cloud-Self-Healing-for-Secure-Containers/monitoring/prometheus.yaml:/etc/prometheus/prometheus.yml   -v /workspaces/Cloud-Self-Healing-for-Secure-Containers/monitoring/alert_rules.yml:/etc/prometheus/alert_rules.yml   prom/prometheus:v2.52.0",
     "kubectl get pods -n monitoring"
 ]
 
